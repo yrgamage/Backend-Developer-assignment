ac372f4f6343a7f893072a1b987bdd398187826f
Yoshani Gamage <157863413+yrgamage@users.noreply.github.com>
2025-06-11 16:46:27 +0530

    Remove email format validation regex from user schema

--- models/UserModel.js
+++ models/UserModel.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
     type: String, 
     required: true, 
     unique: true,
-    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address']
   },
   password: { type: String, required: true }
 });
